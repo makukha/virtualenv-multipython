d028fc1375c628d3559897a09d1d39ffebf19b2f
Michael Makukha <m.makukha@gmail.com>
2025-01-11 19:24:20 +0200

    Add docs generation scripts

--- docsubfile.py
+++ docsubfile.py
@@ -1,47 +1,106 @@
 import json
+from pathlib import Path
 from shlex import split
 from subprocess import check_output
-import sys
+
+import cyclopts
 
 
 IMG = 'makukha/multipython:unsafe'
-BAKE = 'tests/docker-bake.hcl'
-TAG_REMOVE = 'py20'
+BAKEFILE = 'tests/docker-bake.hcl'
+REPORTS_DIR = Path('tests/reports')
+
 POS = '✅'
 NEG = '🚫'
 
-# ruff: noqa: S603 = check_output()
+app = cyclopts.App()
+
+# ruff: noqa: S603 = allow check_output with arbitrary cmdline
 
 
-def print_table(venv_spec: str) -> None:
+@app.command
+def gen_reports() -> None:
+    """
+    Generate test reports.
+    """
     # get source data
+    data = json.loads(check_output(split(f'docker buildx bake -f {BAKEFILE} --print')))
     tags = check_output(
         split(f'docker run --rm {IMG} py ls --tag'),
         text=True,
     ).splitlines()
-    bake = json.loads(check_output(split(f'docker buildx bake -f {BAKE} --print')))
 
-    # make table
-    def table_row(row: dict) -> dict:
+    # generate reports
+    T = 'tox>=4,<5'
+    V = 'virtualenv>=20'
+    for bake_group, venv_pin, desc, name in (
+        ('without_tox', '>=20', f'`{V}`', 'venv'),
+        ('without_tox', '>=20,<20.27', f'`{V},<20.27`', 'venv27'),
+        ('without_tox', '>=20,<20.22', f'`{V},<20.22`', 'venv22'),
+        ('with_tox4', '>=20', f'`{T}`, `{V}`', 'tox4_venv'),
+        ('with_tox4', '>=20,<20.27', f'`{T}`, `{V},<20.27`', 'tox4_venv27'),
+        ('with_tox4', '>=20,<20.22', f'`{T}`, `{V},<20.22`', 'tox4_venv22'),
+    ):
+        REPORTS_DIR.mkdir(parents=True, exist_ok=True)
+        write_report(data, tags, bake_group, venv_pin, desc, name, skip=('py20',))
+
+
+def write_report(
+    data: dict,
+    tags: list[str],
+    bake_group: str,
+    venv_pin: str,
+    desc: str,
+    name: str,
+    skip: tuple[str, ...],
+) -> None:
+    def host_tag_results(args: dict) -> tuple[str, list[str]]:
         marks = [
-            *((t, True) for t in row['TAGS_PASSING'].split()),
-            *((t, False) for t in set(row['TAGS_NOTFOUND'].split()) - {TAG_REMOVE}),
+            *((t, 'P') for t in args['TAGS_PASSING'].split()),
+            *((t, 'F') for t in set(args['TAGS_NOTFOUND'].split()) - set(skip)),
         ]
         marks.sort(key=lambda tm: tags.index(tm[0]))
-        return dict(tag=row['TOX_TAG'], marks=marks)
+        return (args['PYTHON_TAG'], [tm[1] for tm in marks])
 
-    table = [
-        table_row(t['args'])
-        for t in bake['target'].values()
-        if t['args']['VIRTUALENV_PIN'] == venv_spec
-    ]
-    table.sort(key=lambda row: tags.index(row['tag']))
+    targets = data['group'][bake_group]['targets']
+    args = [data['target'][t]['args'] for t in targets]
+    table = [host_tag_results(a) for a in args if a['VIRTUALENV_PIN'] == venv_pin]
+    table.sort(key=lambda row: tags.index(row[0]))
+    results = dict(desc=desc, target_tags=tags, host_tag_results=dict(table))
+    with (REPORTS_DIR / f'{name}.json').open('wt') as f:
+        json.dump(results, f, indent=2)
 
-    # print html
-    for row in table:
-        vals = ''.join(f'<td>{POS if tm[1] else NEG}</td>' for tm in row['marks'])
-        print(f'<tr><th><code>{row['tag']}</code></th>{vals}</tr>')
+
+@app.command
+def pretty_report(name: str) -> None:
+    """
+    Print report in compact terminal-based format.
+
+    Parameters
+    ----------
+    name: str
+        Test report name.
+    """
+    ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVW'
+
+    with (REPORTS_DIR / f'{name}.json').open() as f:
+        data = json.load(f)
+    rowt = 'HOST'
+    colt = 'TARGETS'
+    rowh = data['host_tag_results'].keys()
+    colh = data['target_tags']
+
+    if max(len(colh), len(rowh)) > len(ALPHA):
+        raise RuntimeError('Too many tags')
+
+    width = max(len(rowt), max(len(v) for v in rowh))
+
+    print(f'{" " * width}    {colt}')
+    print(f'{rowt: >{width}}    {" ".join(ALPHA[:len(colh)])}')
+    for i, htag in enumerate(rowh):
+        marks = [{'P': POS, 'F': NEG}[x] for x in data['host_tag_results'][htag]]
+        print(f'{htag: >{width}}  {ALPHA[i]} {"".join(marks)}')
 
 
 if __name__ == '__main__':
-    print_table(venv_spec=sys.argv[1])
+    app()
